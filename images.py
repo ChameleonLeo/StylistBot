02c597deebf15b61221f7ef4ad2e0de901b3dc7f
ChameleonLeo <xena94@mail.ru>
2021-06-28 21:23:42 +0300

    Deploying to Heroku server v2

--- images.py
+++ images.py
@@ -44,8 +44,8 @@ async def set_random_default_set():
 # Func for transformating image to tensor
 async def image_loader(image_name):
     loader = transforms.Compose([
-        transforms.Resize(384),
-        transforms.CenterCrop(384),
+        transforms.Resize(224),  # 384
+        transforms.CenterCrop(224),
         transforms.ToTensor()])
     image = Image.open(image_name)
     image = loader(image).unsqueeze(0)
